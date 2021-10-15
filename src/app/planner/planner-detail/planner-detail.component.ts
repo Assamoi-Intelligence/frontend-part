b37f6b6801fe9e426485a8838e3ac1c0a4bc9c18
Assamoi-Intelligence <56390939+Assamoi-Intelligence@users.noreply.github.com>
2021-10-15 10:53:02 +0000

    fix date of starting service modal

--- src/app/planner/planner-detail/planner-detail.component.ts
+++ src/app/planner/planner-detail/planner-detail.component.ts
@@ -155,7 +155,7 @@ export class PlannerDetailComponent implements OnInit {
       if(status === 'OK') {
         let timestart = orders.find(el => el.id === route[index])?.timewindowstart as number;
         let timeend = orders.find(el => el.id === route[index])?.timewindowend;
-        this.currentTime += (result.rows[0].elements[0].duration.value * 1000) as number;
+        this.currentTime = (this.currentTime+(result.rows[0].elements[0].duration.value * 1000)) as number;
         if(this.currentTime < timestart) {
             this.currentTime = timestart
         }
